4394019be5f35a17a79bbf3bda7cc9469da654f3
awave1 <artgolovin96@gmail.com>
2020-12-02 20:44:20 -0700

    rename day_one -> day01

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-mod day_one;
+mod day01;
 
 fn main() {
-    println!("day one (pt1): {:?}", day_one::part_one(false));
-    println!("day one (pt2): {:?}", day_one::part_two(false));
+    println!("day one (pt1): {:?}", day01::part_one(false));
+    println!("day one (pt2): {:?}", day01::part_two(false));
 }
