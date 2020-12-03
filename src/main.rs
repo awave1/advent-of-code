6afa82b0522aee7119612464036abb889bd77a5a
awave1 <artgolovin96@gmail.com>
2020-12-02 20:53:11 -0700

    add cli args to run days

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
 mod day01;
 
 fn main() {
-    println!("day one (pt1): {:?}", day01::part_one(false));
-    println!("day one (pt2): {:?}", day01::part_two(false));
+    let args: Vec<String> = std::env::args().collect();
+
+    match args.get(1) {
+        Some(day) => match day.as_str() {
+            "1" => {
+                println!("day 1.1: {:?}", day01::part_one(false));
+                println!("day 1.2: {:?}", day01::part_two(false));
+            }
+            _ => panic!("day is not supported"),
+        },
+        None => panic!("day is not specified"),
+    }
 }
--- src/solution.rs
+++
@@ -1,4 +0,0 @@
-pub trait Solution<T> {
-    fn part_one(use_test: bool) -> Option<T>;
-    fn part_two(use_test: bool) -> Option<T>;
-}
