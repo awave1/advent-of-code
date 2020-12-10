f13b633e071cc90c360679944c15fa5181d02b01
awave1 <artgolovin96@gmail.com>
2020-12-09 19:49:09 -0700

    finish day 2

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
 mod day01;
+mod day02;
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
+    let is_test = (args.get(2).unwrap_or(&String::from(""))).eq("test");
 
     match args.get(1) {
         Some(day) => match day.as_str() {
             "1" => {
-                println!("day 1.1: {:?}", day01::part_one(false));
-                println!("day 1.2: {:?}", day01::part_two(false));
+                println!("day 1.1: {:?}", day01::part_one(is_test));
+                println!("day 1.2: {:?}", day01::part_two(is_test));
+            }
+            "2" => {
+                println!("day 2.1: {:?}", day02::part_one(is_test));
+                println!("day 2.2: {:?}", day02::part_two(is_test));
             }
             _ => panic!("day is not supported"),
         },
