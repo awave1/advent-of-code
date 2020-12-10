18326620ad067cc6c03de244398a172045726a71
Artem Golovin <artgolovin96@gmail.com>
2020-12-09 23:12:24 -0700

    improve cli arguments

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,42 @@
+use clap;
+
 mod day01;
 mod day02;
 
-fn main() {
-    let args: Vec<String> = std::env::args().collect();
-    let is_test = (args.get(2).unwrap_or(&String::from(""))).eq("test");
+fn parse_args() -> clap::ArgMatches {
+    clap::App::new("advent-of-code")
+        .arg(
+            clap::Arg::new("day")
+                .short('d')
+                .long("day")
+                .value_name("DAY")
+                .about("Specifies the day to run solutions for")
+                .takes_value(true)
+                .required(true),
+        )
+        .arg(
+            clap::Arg::new("test")
+                .short('t')
+                .long("test")
+                .about("Runs only test inputs"),
+        )
+        .get_matches()
+}
 
-    match args.get(1) {
-        Some(day) => match day.as_str() {
-            "1" => {
-                println!("day 1.1: {:?}", day01::part_one(is_test));
-                println!("day 1.2: {:?}", day01::part_two(is_test));
-            }
-            "2" => {
-                println!("day 2.1: {:?}", day02::part_one(is_test));
-                println!("day 2.2: {:?}", day02::part_two(is_test));
-            }
-            _ => panic!("day is not supported"),
-        },
-        None => panic!("day is not specified"),
+fn main() {
+    let args = parse_args();
+    let day = args.value_of("day").unwrap();
+    let is_test = args.occurrences_of("test") == 1;
+
+    match day {
+        "1" => {
+            println!("day 1.1: {:?}", day01::part_one(is_test));
+            println!("day 1.2: {:?}", day01::part_two(is_test));
+        }
+        "2" => {
+            println!("day 2.1: {:?}", day02::part_one(is_test));
+            println!("day 2.2: {:?}", day02::part_two(is_test));
+        }
+        _ => panic!("solutions for day {} are not implemented", day),
     }
 }
