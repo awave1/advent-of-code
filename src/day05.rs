55b28999740f57862951304a8384685c03e4dfc4
awave1 <artgolovin96@gmail.com>
2020-12-26 19:28:49 -0700

    add day 5

---
+++ src/day05.rs
@@ -0,0 +1,19 @@
+use crate::input::get_input;
+
+pub fn part_one(use_test: bool) -> Option<u32> {
+  None
+}
+
+pub fn part_two(use_test: bool) -> Option<u32> {
+  None
+}
+
+#[test]
+fn part_one_test() {
+  assert_eq!(None, part_one(true))
+}
+
+#[test]
+fn part_two_test() {
+  assert_eq!(None, part_two(true))
+}
